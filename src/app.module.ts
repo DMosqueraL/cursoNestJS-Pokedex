33ba6655a564bfb412c934a659905ce56308c58a
Doris Mosquera Lozano <doris.mosquera@sofka.local>
2022-08-22 10:51:10 -0500

    Proyecto antes de insertsr por lotes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module({
@@ -17,7 +18,9 @@ import { CommonModule } from './common/common.module';
     
     PokemonModule,
     
-    CommonModule
+    CommonModule,
+    
+    SeedModule
   ],
   controllers: [],
   providers: [],
